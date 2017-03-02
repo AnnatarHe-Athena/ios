aa4356aee83de9d0403db2aea1d39a34b90618d7
AnnatarHe <iamhele1994@gmail.com>
2017-03-02 21:31:03 +0800

    Initial Commit

---
+++ dbmeinv-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  dbmeinv-ios
+//
+//  Created by 贺乐 on 02/03/2017.
+//  Copyright © 2017 贺乐. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
