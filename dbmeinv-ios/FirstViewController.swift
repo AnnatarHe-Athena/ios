04600ad57e3786f5d0e96166bf0c500acbabfee6
AnnatarHe <iamhele1994@gmail.com>
2017-03-03 23:14:55 +0800

    request categories

--- dbmeinv-ios/FirstViewController.swift
+++ dbmeinv-ios/FirstViewController.swift
@@ -9,28 +9,61 @@
 import UIKit
 import Alamofire
 
+struct CategoryItem {
+    let id: Int
+    let name: String
+    let src: Int
+}
+
 class FirstViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var categories: [CategoryItem?] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
         self.requestAllCategroies()
     }
     
     func requestAllCategroies() {
-        print("start request")
         Alamofire.request("https://db.annatarhe.com/api/meinv/categories").responseJSON { response in
-            print("requst end")
-            print(response.result.value!)
+            if let result = response.result.value {
+                if let categories = result as? [NSDictionary] {
+                    for var i in 0..<categories.count {
+                        let id = categories[i]["id"]! as! Int
+                        let name = categories[i]["name"]! as! String
+                        let src = categories[i]["src"]! as! Int
+                        self.categories.append(CategoryItem(id: id, name: name, src: src))
+                    }
+                    self.tableView.reloadData()
+                }
+            }
         }
-        print("requesting")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension FirstViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NormalCell", for: indexPath)
+        if let name = categories[indexPath.row]?.name {
+            cell.textLabel?.text = name
+        } else {
+            cell.textLabel?.text = "nil"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+}
